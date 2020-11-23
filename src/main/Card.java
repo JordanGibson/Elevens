59cb7bc1e0ec74d6995d74bb4b80e15debe05684
Jordan Gibson <34385202+JordanGibson@users.noreply.github.com>
2020-11-23 13:48:38 +0000

    Reorder cards

--- src/main/Card.java
+++ src/main/Card.java
@@ -6,7 +6,7 @@ public class Card implements Comparable<Card> {
     private final int rank, suit;
     private static final Random GENERATOR = new Random();
     private static final String[] RANKS = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-    private static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    private static final String[] SUITS = {"Clubs", "Hearts", "Spades", "Diamonds"};
 
     public Card() {
         rank = GENERATOR.nextInt(RANKS.length);
